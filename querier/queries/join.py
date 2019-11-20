f33ea377054aa39356ef36a7adc7014d03c9d205
Thierry Moudiki <thierry.moudiki@gmail.com>
2019-11-20 21:29:06 +0100

    update join

--- querier/queries/join.py
+++ querier/queries/join.py
@@ -21,7 +21,10 @@ from ..utils import memoize
 # join(df1, df2, 'key', "right")
 # join(df1, df2, 'key', "outer")
 def join(df1, df2, on=None, type_join="inner", **kwargs):
+    
+    if on is not None: 
+        on_ = on.replace(" ", "").split(',')
 
     return pd.merge(
-        df1, df2, on=on, how=type_join, **kwargs
+        df1, df2, on=on_, how=type_join, **kwargs
     )
