3adf79d19a3e3ff7e43407382746cdcf3c4d1372
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-05-29 09:35:39 +0200

    update conn.execute

--- querier/queries/request.py
+++ querier/queries/request.py
@@ -57,7 +57,8 @@ def request(df, req=None, **kwargs):
 
         engine = create_engine("sqlite://", echo=False)
         df.to_sql("df", con=engine, **kwargs)
-        req_res = engine.execute(req.upper()).fetchall()
+        with engine.connect() as conn:
+            req_res = conn.execute(req.upper()).fetchall()
         col_names = parse_cols_request(req)
         df_res = pd.DataFrame(req_res, columns=col_names)
 
